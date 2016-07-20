a218ac76fdd0059957bdd6c4716faa1696829cc2
Björn Büttner <Idrinth@users.noreply.github.com>
2016-07-20 15:03:01 +0200

    moving list building to tier removing duplicate content from tier

--- mods/tier.js
+++ mods/tier.js
@@ -29,6 +29,73 @@ idrinth.tier = {
         }
     },
     getTierForName: function ( name ) {
+        var makeList= function ( list ) {
+            var makeField = function ( listKey, difficulty, ic ) {
+                var ln = { type: 'td' };
+                try {
+                    ln.styles = idrinth.tier.list[listKey].os[difficulty] === idrinth.tier[listKey][difficulty][ic] ? 'is-os' : '';
+                } catch ( E ) {
+                    idrinth.log ( E.toString ( ) );
+                }
+                try {
+                    ln.content = idrinth.ui.formatNumber ( idrinth.tier.list[listKey][difficulty][ic] ) + ' ' +
+                            idrinth.tier.list[listKey].epics[difficulty][ic] + 'E';
+                } catch ( E2 ) {
+                    idrinth.log ( E2.toString ( ) );
+                    try {
+                        ln.content = idrinth.ui.formatNumber ( idrinth.tier.list[listKey][difficulty][ic] );
+                    } catch ( E3 ) {
+                        idrinth.log ( E3.toString ( ) );
+                    }
+                }
+                return ln;
+            };
+            var makeRow = function(title,dataset) {
+                return { type: 'tr', children: [
+                            { type: 'th', content: title },
+                            { type: 'td', content: idrinth.ui.formatNumber ( dataset.n ) },
+                            { type: 'td', content: idrinth.ui.formatNumber ( dataset.h ) },
+                            { type: 'td', content: idrinth.ui.formatNumber ( dataset.l ) },
+                            { type: 'td', content: idrinth.ui.formatNumber ( dataset.nm ) }
+                        ] };
+            };
+            var wrapper = document.getElementById ( 'idrinth-tierlist' );
+            wrapper.innerHTML = '';
+            for (var count = list.length - 1; count >= 0; count--) {
+                var sub = idrinth.ui.buildElement ( { type: 'div', css: 'tier-wrapper', children: [
+                        { type: 'img', attributes: [ { name: 'src', value: 'https://dotd.idrinth.de/static/raid-image-service/' + idrinth.tier.list[list[count]].url + '/' } ] },
+                        { type: 'strong', content: idrinth.tier.list[list[count]].name },
+                        { type: 'span', content: 'AP: ' + idrinth.ui.formatNumber ( idrinth.tier.list[list[count]].ap ) },
+                        { type: 'table', children: [
+                                { type: 'thead', children: [
+                                        { type: 'tr', children: [
+                                                { type: 'th', content: '#' },
+                                                { type: 'th', content: 'Normal' },
+                                                { type: 'th', content: 'Hard' },
+                                                { type: 'th', content: 'Legend' },
+                                                { type: 'th', content: 'Nightmare' }
+                                            ] }
+                                    ] },
+                                { type: 'tbody', children: [
+                                        makeRow('FS',idrinth.tier.list[list[count]].fs),
+                                        makeRow('OS',idrinth.tier.list[list[count]].os),
+                                        makeRow('',{n:'',l:'',h:'',nm:''})
+                                    ] }
+                            ] }
+                    ] } );
+                var maxTiers = Math.max ( idrinth.tier.list[list[count]].n.length, idrinth.tier.list[list[count]].h.length, idrinth.tier.list[list[count]].l.length, idrinth.tier.list[list[count]].nm.length );
+                for (var ic = 0; ic < maxTiers; ic++) {
+                    sub.lastChild.lastChild.appendChild ( idrinth.ui.buildElement ( { type: 'tr', children: [
+                            { type: 'th', content: ic + 1 },
+                            makeField ( list[count], 'n', ic ),
+                            makeField ( list[count], 'h', ic ),
+                            makeField ( list[count], 'l', ic ),
+                            makeField ( list[count], 'nm', ic )
+                        ] } ) );
+                }
+                wrapper.appendChild ( sub );
+            }
+        };
         var result = [ ];
         var regExp = new RegExp ( name, 'i' );
         for (var key in idrinth.tier.list) {
@@ -36,6 +103,6 @@ idrinth.tier = {
                 result.push ( key );
             }
         }
-        idrinth.ui.makeTierList ( result );
+        makeList ( result );
     }
 };
