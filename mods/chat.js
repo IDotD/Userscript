54c29c9e9dcb4024abcabbf289356853d8fcd586
Björn Büttner <Idrinth@users.noreply.github.com>
2016-07-21 10:55:25 +0200

    doing the cleanup I ment
    - no continue
    - check for own property

--- mods/chat.js
+++ mods/chat.js
@@ -346,7 +346,9 @@ idrinth.chat = {
                         chat.removeChild(chat.firstChild);
                     }
                     for (var userId in idrinth.chat.chatRank[chatId]) {
-                        addMemberElement(chat, chatId, userId);
+-                       if (idrinth.chat.chatRank[chatId].hasOwnProperty(userId)) {
+                            addMemberElement(chat, chatId, userId);
+ -                      }
                     }
                 }
             }
