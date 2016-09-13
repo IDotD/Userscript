a7ae54a3069a128ab437af742f1ae72481d919f3
Björn Büttner <Idrinth@users.noreply.github.com>
2016-09-13 15:57:05 +0200

    making the fallback slightly better readable

--- src/libs/matches-selector-polyfill.js
+++ src/libs/matches-selector-polyfill.js
@@ -1,18 +1,20 @@
 /**
  * from https://developer.mozilla.org/de/docs/Web/API/Element/matches
  */
-if ( !Element.prototype.matches ) {
-    Element.prototype.matches =
-            Element.prototype.matchesSelector ||
-            Element.prototype.mozMatchesSelector ||
-            Element.prototype.msMatchesSelector ||
-            Element.prototype.oMatchesSelector ||
-            Element.prototype.webkitMatchesSelector ||
-            function ( s ) {
-                var matches = ( this.document || this.ownerDocument ).querySelectorAll ( s ),
-                        i = matches.length;
-                while ( --i >= 0 && matches.item ( i ) !== this ) {
+Element.prototype.matches =
+        Element.prototype.matches ||
+        Element.prototype.matchesSelector ||
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.oMatchesSelector ||
+        Element.prototype.webkitMatchesSelector ||
+        function ( s ) {
+            var matches = ( this.document || this.ownerDocument ).querySelectorAll ( s );
+            var i = matches.length;
+            while ( --i >= 0 ) {
+                if ( matches.item ( i ) === this ) {
+                    true;
                 }
-                return i > -1;
-            };
-}
+            }
+            return false;
+        };
