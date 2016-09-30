7801250cee7b212e41dc25c0166ab0a8f0da3221
ardenian <florian.graf.khe@gmail.com>
2016-09-30 16:35:38 +0200

    progress

--- src/mods/text.js
+++ src/mods/text.js
@@ -1,6 +1,7 @@
 idrinth.text = {     
     data: {
         chat: {
+            user:{
                 ban: 'Ban User',
                 banned: 'Banned',
                 makeMod: 'Make Moderator',
@@ -8,39 +9,43 @@ idrinth.text = {
                 owner: 'Owner',
                 makeUser: 'Make user',
                 user: 'User',
-                invite:'Invite to Chat ',
+            },
+            button:{
                 close: 'Close',
+                invite:'Invite to Chat ',
+                account: 'Account',
+                gameReload: 'Reload game',
+                scriptReload: 'Reload Script',
+                facebookLoginRefresh: 'Refresh Facebook Game Login',
+                ngRaidJoin: 'NG Raid Join(slow!)',
+                manually: 'Import all manually',
+                fav: 'Import favs manually',
+                disableAutojoin: 'disable timed Autojoin',
+                enableAutojoin: 'enable timed Autojoin',
+                clear: 'Clear Raids',
+                joinRestart: 'Restart Raidjoin',
+                joinedLast: 'Last raids joined:',
+                bossName: 'Enter Boss\' Name'
+            },
+            message:{
+                optional: 'This part of the script is optional, so logging in is unneeded for raid catching etc.',
+                invalid: 'This should not be the data for logging in on the related gaming site and the login does not need to match your ingame name - you can set a display name after the registration.',
+                offline: "Not logged in, click to login/register",
+                modifyFail:'Can\'t modify that user at the moment',
+                joinFail:'Can\'t create at the moment',
+                joinNotwork:'Joining didn\'t work',
+                loginFail: 'Login failed in an unexpected way',
                 unknown:'The given username for dotd.idrinth.de is unknown, do you want to register it there?',
-                
-                'modify.fail':'Can\'t modify that user at the moment',
-                'join.fail':'Can\'t create at the moment',
-                'join.notwork':'Joining didn\'t work',
-                'chat.join.notwork': 'Joining didn\'t work',
-                'chat.login.fail': 'Login failed in an unexpected way',
-                'default.error': 'Unexpected error occurred. Please contact script developers'
+            },
+            error:{
+                errorDefault: 'Unexpected error occurred. Please contact script developers'
                                 + ' (https://github.com/Idrinth/IDotD).',
-                'This part of the script is optional, so logging in is unneeded for raid catching etc.': 'This part of the script is optional, so logging in is unneeded for raid catching etc.',
-                'Account': 'Account',
-                'This should not be the data for logging in on the related gaming site and the login does not need to match your ingame name - you can set a display name after the registration.': 'This should not be the data for logging in on the related gaming site and the login does not need to match your ingame name - you can set a display name after the registration.',
-                "Not logged in, click to login/register": "Not logged in, click to login/register",
-                'raid.import.manually.all': 'Import all manually',
-                'raid.import.manually.fav': 'Import favs manually',
-                'game.reload': 'Reload game',
-                'raid.clear': 'Clear Raids',
-                'script.reload': 'Reload Script',
-                'raid.join.restart': 'Restart Raidjoin',
-                'game.facebook.login.refresh': 'Refresh Facebook Game Login',
-                'NG Raid Join(slow!)': 'NG Raid Join(slow!)',
-                'disable timed Autojoin': 'disable timed Autojoin',
-                'enable timed Autojoin': 'enable timed Autojoin',
-                'Last raids joined:': 'Last raids joined:',
-                'Enter Boss\' Name': 'Enter Boss\' Name'
-
-                }
-        },
+           }
+        }
+    },
     get: function (key) {
         var textKey = key || '';
         var text=idrinth.text.data;
-        return text.hasOwnProperty(textKey) ? data[textKey] : text['default.error'];
+        return text.hasOwnProperty(textKey) ? data[key] : text['default.error'];
     }
 }
