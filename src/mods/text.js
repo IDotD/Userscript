43099c12addb0faa57a9e86916a00c84ec73cb35
Ardenian <florian.graf.khe@gmail.com>
2016-09-30 14:32:07 +0200

    collected text

--- src/mods/text.js
+++ src/mods/text.js
@@ -1,53 +1,37 @@
 ﻿idrinth.text = {
     getText: function (key) {
         var textKey = key || '';
-        var text = {
+        var data = {
+            "chat.user.action.ban": "Ban user",
+            'chat.user.status.banned': 'Banned',
+            'chat.user.action.makeMod': 'Make Moderator',
+            'chat.user.action.makeAdmin': 'Make Admin',
+            'chat.user.owner': 'Owner',
+            'chat.user.action.makeUser': 'Make User',
+            'chat.user.user': 'User',
+            'chat.user.action.invite ': 'Invite to Chat ',
+            'chat.user.action.close': 'Close',
 
-            // currently under progress
-
-            // Texts from chat.js
-            // TODO ? - Remove getMsg function in idrinth.chat and call text via key directly from idrinth.text
-            // TODO ? - Replace hardcoded strings with function calls to idrinth.text
-
-            'Ban User': 'Ban User',
-            'Banned': 'Banned',
-            'Make Moderator': 'Make Moderator',
-            'Make Admin': 'Make Admin',
-            'Owner': 'Owner',
-            'Make User': 'Make User',
-            'User': 'User',
-
-            'Invite to Chat ': 'Invite to Chat ',
-            //'User':'User', - only reminder there is something to change in chat.js
-
-            'Close': 'Close',
-
-            // line 439 - only reminder there is something to change in chat.js
+            'chat.user.modify.fail': 'Can\'t modify that user at the moment',
+            'chat.create.fail': 'Can\'t create at the moment',
+            'chat.join.fail': 'Can\'t join at the moment',
+            'chat.join.notwork': 'Joining didn\'t work',
+            'chat.user.status.unknown': 'The given username for dotd.idrinth.de is unknown, do you want to register it there?',
+            'chat.login.fail': 'Login failed in an unexpected way',
+            'default.error': 'Unexpected error occurred. Please contact script developers'
+                    + ' (https://github.com/Idrinth/IDotD).',
 
             'This part of the script is optional, so logging in is unneeded for raid catching etc.': 'This part of the script is optional, so logging in is unneeded for raid catching etc.',
             'Account': 'Account',
             'This should not be the data for logging in on the related gaming site and the login does not need to match your ingame name - you can set a display name after the registration.': 'This should not be the data for logging in on the related gaming site and the login does not need to match your ingame name - you can set a display name after the registration.',
-
             "Not logged in, click to login/register": "Not logged in, click to login/register",
 
+            'raid.import.manually.all': 'Import all manually',
+            'raid.import.manually.fav': 'Import favs manually',
 
-
-            'modify.fail': 'Can\'t modify that user at the moment',
-            'create.fail': 'Can\'t create at the moment',
-            'join.fail': 'Can\'t join at the moment',
-            'join.notwork': 'Joining didn\'t work',
-            'user.unknown': 'The given username for dotd.idrinth.de is unknown, do you want to register it there?',
-            'login.fail': 'Login failed in an unexpected way',
-            'default.error': 'Unexpected error occurred. Please contact script developers'
-                    + ' (https://github.com/Idrinth/IDotD).',
-
-            // Texts from ui.js
-            // TODO ? - Replace hardcoded strings with function calls to idrinth.text
-            'Import all manually': 'Import all manually',
-            'Import favs manually': 'Import favs manually',
-            'Reload game': 'Reload game',
-            'Clear Raids': 'Clear Raids',
-            'Reload Script': 'Reload Script',
+            'reload.game': 'Reload game',
+            'raid.clear': 'Clear Raids',
+            'reload.script': 'Reload Script',
             'Restart Raidjoin': 'Restart Raidjoin',
             'Refresh Facebook Game Login': 'Refresh Facebook Game Login',
             'NG Raid Join(slow!)': 'NG Raid Join(slow!)',
@@ -56,9 +40,9 @@
 
             'Last raids joined:': 'Last raids joined:',
             'Enter Boss\' Name': 'Enter Boss\' Name'
-
-
         }
-        return text.hasOwnProperty(textKey) ? text[textKey] : text['default.error'];
+
+
+        return text.hasOwnProperty(textKey) ? data[textKey] : text['default.error'];
     }
 }
