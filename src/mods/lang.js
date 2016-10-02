ebb94b14432cbff9d5e9d412ce136c586e7dbe98
ardenian <florian.graf.khe@gmail.com>
2016-10-02 19:01:46 +0200

    First lang.js awake lines

--- src/mods/lang.js
+++ src/mods/lang.js
@@ -1,3 +1,11 @@
 ﻿idrinth.lang = {
-
+    awake: function () {
+        var language = window.navigator.userLanguage || window.navigator.language;
+        alert(language);
+        var file = "" ; // JSON to use, dependent on language
+        JSON.parse(file);
+        for (prop in file) {
+            idrinth.lang.prop = file[prop];
+        }
+    }
 }
