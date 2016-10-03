bb45337595c7fb65f13a28e60795db16ecee6c7f
Denis Denisov <immortalwibb@gmail.com>
2016-10-03 15:40:17 +0300

    #125: added first dummy test and configs;

---
+++ tests/mods/land.js
@@ -0,0 +1,12 @@
+describe ( 'Land.js tests', function () {
+
+    it('should have a idrinth variable in scope', function() {
+        expect(idrinth).toBeDefined();
+    });
+
+
+    it ( 'should be true', function () {
+        expect ( 'foo' ).toBe ( 'foo' );
+    } );
+
+} );
