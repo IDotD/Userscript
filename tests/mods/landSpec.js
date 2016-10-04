8896b53e57f91136cf20899eb0d4d79c34b9b274
Denis Denisov <immortalwibb@gmail.com>
2016-10-04 11:08:31 +0300

    #125: land test for default data;

--- tests/mods/landSpec.js
+++ tests/mods/landSpec.js
@@ -1,12 +1,55 @@
 describe ( 'Land.js tests', function () {
 
-    it('should have a idrinth variable in scope', function() {
-        expect(idrinth).toBeDefined();
-    });
+    it ( 'should have a idrinth variable in scope', function () {
+        expect ( idrinth ).toBeDefined ();
+    } );
 
 
-    it ( 'should be true', function () {
-        expect ( 'foo' ).toBe ( 'foo' );
+    it ( 'should check default data is valid', function () {
+        var defData = {
+            cornfield: {
+                perHour: 100,
+                base: 4000
+            },
+            stable: {
+                perHour: 300,
+                base: 15000
+            },
+            barn: {
+                perHour: 400,
+                base: 25000
+            },
+            store: {
+                perHour: 700,
+                base: 50000
+            },
+            pub: {
+                perHour: 900,
+                base: 75000
+            },
+            inn: {
+                perHour: 1200,
+                base: 110000
+            },
+            tower: {
+                perHour: 2700,
+                base: 300000
+            },
+            fort: {
+                perHour: 4500,
+                base: 600000
+            },
+            castle: {
+                perHour: 8000,
+                base: 1200000
+            }
+        };
+
+        expect ( idrinth.land.data ).toEqual ( defData );
+    } );
+
+    lt ( "", function () {
+
     } );
 
 } );
