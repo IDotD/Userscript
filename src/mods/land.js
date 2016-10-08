025a3787d9e14c5b5c430f76e6f29a40428850ea
Björn Büttner <Idrinth@users.noreply.github.com>
2016-10-08 17:41:46 +0200

    reformatting

--- src/mods/land.js
+++ src/mods/land.js
@@ -70,7 +70,7 @@ idrinth.land = {
         }
         var results = baseCalculator ( getRequirements () );
         if ( Object.keys ( results ).length === 0 ) {
-            idrinth.core.alert( idrinth.text.get("land.lackgold") );
+            idrinth.core.alert ( idrinth.text.get ( "land.lackgold" ) );
         }
         putResults ( results );
     },
