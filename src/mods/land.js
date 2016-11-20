fa24002b0cae6b4e1e9b26083151525fb4c7c4eb
Björn Büttner <Idrinth@users.noreply.github.com>
2016-11-20 12:35:06 +0100

    fiing another condition

--- src/mods/land.js
+++ src/mods/land.js
@@ -67,7 +67,7 @@ idrinth.land = {
             document.getElementById ( 'idrinth-land-gold' ).value = idrinth.settings.get ( "land#gold" );
         };
         for (var key in idrinth.settings.get ( "land", true )) {
-            if ( idrinth.settings.get ( "land#" + hasOwnProperty ( key ) ) ) {
+            if ( idrinth.settings.get ( "land#" + key ) ) {
                 idrinth.settings.change ( 'land#' + key, parseInt ( document.getElementById ( 'idrinth-land-' + key ).value, 10 ) );
             }
         }
