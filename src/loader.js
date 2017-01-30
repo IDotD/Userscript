49861dce4dbf95618d09957a4ae5175405749bd3
Björn Büttner <Idrinth@users.noreply.github.com>
2017-01-30 21:04:50 +0100

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -27,23 +27,24 @@
 (function () {
   var sc = document.createElement ( 'script' );
   sc.setAttribute ( 'src', 'https://dotd.idrinth.de/static/userscript/' + GM_info.script.version + '/' );
-  sc.setAttribute( 'async', 'async' );
+  sc.setAttribute ( 'id', 'idotd-loader' );
+  sc.setAttribute ( 'async', 'async' );
   sc.errorCounter = 0;
   sc.errorFunction = function(){
-      this.parentNode.removeChild(this);
+      var self = document.getElementById( 'idotd-loader' );
+      self.parentNode.removeChild(self);
       var sc = document.createElement ( 'script' );
-      sc.onerror=this.onerror;
-      sc.errorCounter = sc.errorCounter + 1;
-      sc.errorFunction=this.errorFunction;
-      sc.errorFunction.bind(sc);
+      sc.onerror = self.onerror;
+      sc.errorCounter = self.errorCounter + 1;
+      sc.errorFunction = self.errorFunction;
+      sc.setAttribute ( 'id', 'idotd-loader' );
       sc.setAttribute( 'async', 'async' );
-      sc.setAttribute ( 'src', this.getAttribute( 'src' ) + Math.random() + '/' );
+      sc.setAttribute ( 'src', (self.getAttribute( 'src' )+'').replace(/(userscript\/.*?\/).*$/,'$1') + Math.random() + '/' );
       document.getElementsByTagName ( 'head' )[0].appendChild ( sc );
   };
-  sc.errorFunction.bind( sc );
   sc.onerror=function(){
-      console.log("Failed loading IDotD, retry in " + (500+100*this.errorCounter/1000) + "sec." );
-      window.setTimeout( this.errorFunction, 500+100*this.errorCounter );
+      console.log("Failed loading IDotD, retry in " + (500+100*this.errorCounter*this.errorCounter)/1000 + "sec." );
+      window.setTimeout( this.errorFunction, 500+100*this.errorCounter*this.errorCounter );
   };
   document.getElementsByTagName ( 'head' )[0].appendChild ( sc );
 }());
