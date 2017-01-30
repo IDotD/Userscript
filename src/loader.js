6845f30db95edc858ca1889c8a729112c5b82e11
Björn Büttner <Idrinth@users.noreply.github.com>
2017-01-30 17:30:06 +0100

    Create loader.js
    adding loader to allow edits

---
+++ src/loader.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name           Idrinth's DotD Script
+// @description    This script collects Kongregate Usernames to match them to ingame names and provides autojoining for raids
+// @author         Idrinth
+// @version        2.2.0
+// @grant          none
+// @hompage        http://dotd.idrinth.de
+// @include        http://www.kongregate.com/games/5thplanetgames/dawn-of-the-dragons*
+// @include        https://www.kongregate.com/games/5thplanetgames/dawn-of-the-dragons*
+// @include        http://www.kongregate.com/games/5thPlanetGames/dawn-of-the-dragons*
+// @include        https://www.kongregate.com/games/5thPlanetGames/dawn-of-the-dragons*
+// @include        http://kongregate.com/games/5thplanetgames/dawn-of-the-dragons*
+// @include        https://kongregate.com/games/5thplanetgames/dawn-of-the-dragons*
+// @include        http://kongregate.com/games/5thPlanetGames/dawn-of-the-dragons*
+// @include        https://kongregate.com/games/5thPlanetGames/dawn-of-the-dragons*
+// @include        http://www.dawnofthedragons.com/game*
+// @include        http://www.dawnofthedragons.com/
+// @include        http://www.dawnofthedragons.com
+// @include        http://dawnofthedragons.com/game*
+// @include        http://web1.dawnofthedragons.com/live_standalone/*
+// @include        http://www.newgrounds.com/portal/view/609826*
+// @include        http://newgrounds.com/portal/view/609826*
+// @include        http://dawnofthedragons.com/
+// @include        http://armorgames.com/dawn-of-the-dragons-game/13509*
+// @include        http://www.armorgames.com/dawn-of-the-dragons-game/13509*
+// ==/UserScript==
+var sc = document.createElement ( 'script' );
+sc.setAttribute ( 'src', 'https://dotd.idrinth.de/static/userscript/2.0.0/' );
+document.getElementsByTagName ( 'body' )[0].appendChild ( sc );
