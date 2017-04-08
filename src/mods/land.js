c56d294a1774d585bede803ef5cb9a2fd1157d80
Ardenian <florian.graf.khe@gmail.com>
2017-04-08 22:34:44 +0200

    fixes #302 fixes #298 fixes 301

--- src/mods/land.js
+++ src/mods/land.js
@@ -130,7 +130,9 @@ idrinth.land = {
             document.getElementById ( 'idrinth-land-gold' ).value = idrinth.settings.get ( "land#gold" );
         };
         for (var key in idrinth.settings.get ( "land", true )) {
-            idrinth.settings.change ( 'land#' + key, parseInt ( document.getElementById ( 'idrinth-land-' + key ).value, 10 ) );
+            if ( idrinth.settings.get( "land", true ).hasOwnProperty( key ) ) {
+                idrinth.settings.change( 'land#' + key, parseInt( document.getElementById( 'idrinth-land-' + key ).value, 10 ) );
+            }
         }
         var results = baseCalculator ( getRequirements () );
         if ( Object.keys ( results ).length === 0 ) {
